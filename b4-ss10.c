04752e3b0daf5ba44d9245a58ce7230c51af7984
tiepd1112-spec <tiepd1112@gmail.com>
2025-11-04 22:07:17 +0700

    Refactor sorting algorithm to bubble sort with input
    Replaced selection sort with bubble sort and added user input for array size and elements.

--- b4-ss10.c
+++ b4-ss10.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
-int main(){
-	int arr[] = {1,4,29,51,24,3,5,6,2,9,6};
 
-	int n = sizeof(arr)/sizeof(arr[0]);
-	int k,temp;
+int main() {
+    int n, i, j, temp;
 
-	//Sap xep chon truc tiep
-	for(int i=0;i<n-1;i++){
-		k = i;
-		for(int j=i+1;j<n;j++){
-			if(arr[j]<arr[k]){
-				k = j;
-			}
-		}
-		if(k!=i){
-			temp = arr[i];
-			arr[i] = arr[k];
-			arr[k] = temp;
-		}
-	}
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", &n);
 
-	printf("\nMang sap xep tang dan bang thuat toan chon truc tiep\n");
-	for(int i=0;i<n;i++){
-		printf("%d  ",arr[i]);
-	}
+    int a[n];
+    printf("Nhap cac phan tu cua mang:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Bubble Sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Mang sau khi sap xep theo thu tu tang dan: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
 }
 
